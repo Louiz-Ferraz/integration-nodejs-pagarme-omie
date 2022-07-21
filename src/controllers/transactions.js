eb2020ce469a75896d7f55f32dc9696f59cab374
Luiz Ferraz <luizfelipeferraz1@gmail.com>
2022-07-21 15:55:24 -0300

    Endpoints to get transactions and payables

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -14,12 +14,47 @@ const getTransaction = async (req, res) => {
     }
 }
 
-const createTransaction = async (req, res) => {
-    const body = req.body;
+const getAllTransactions = async (req, res) => {
+    const { status } = req.query;
+    instanciaAxios.defaults.params.status = status;
+
+    const today = new Date();
+    const priorDate = new Date(new Date().setDate(today.getDate() - 90));
+    instanciaAxios.defaults.params.date_updated = `>=${+priorDate}`;
+
+    instanciaAxios.defaults.params.count = 1000;
+    instanciaAxios.defaults.params.page = 1;
 
     try {
-        const transaction = await instanciaAxios.post('transactions', body);
-        return res.status(201).json(transaction.data);
+        let transactions = await instanciaAxios.get(`transactions`);
+        let arrayTransactions = transactions.data;
+
+        if(arrayTransactions.length >= 1000) { 
+            while (transactions.data.length >= 1000) {
+                instanciaAxios.defaults.params.page++;
+                transactions = await instanciaAxios.get(`transactions`);
+                for (let item of transactions.data) {
+                    arrayTransactions.push(item);
+                }
+            }
+        }
+
+        return res.status(transactions.status).json(arrayTransactions);
+    } catch (error) {
+        const { status, data: { errors } } = error.response;
+        return res.status(status).json(
+            `${errors[0].type}: ${errors[0].parameter_name} - ${errors[0].message}`
+        );
+    }
+}
+
+const getPayablesTransaction = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const transactions = await instanciaAxios.get(`transactions/${id}/payables`);
+
+        return res.status(transactions.status).json(transactions.data);
     } catch (error) {
         const { status, data: { errors } } = error.response;
         return res.status(status).json(
@@ -30,5 +65,6 @@ const createTransaction = async (req, res) => {
 
 module.exports = {
     getTransaction,
-    createTransaction,
+    getAllTransactions,
+    getPayablesTransaction
 }
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const transactionsController = require('./controllers/transactions');
 const routes = express();
 
 routes.get('/transactions/:id', transactionsController.getTransaction);
-routes.post('/transactions', transactionsController.createTransaction);
+routes.get('/transactions', transactionsController.getAllTransactions);
+routes.get('/transactions/:id/payables', transactionsController.getPayablesTransaction);
 
 module.exports = routes;
