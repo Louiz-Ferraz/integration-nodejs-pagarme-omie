4436ff3ca47de63b31380b992d4bbc118a7896ad
Luiz Ferraz <luizfelipeferraz1@gmail.com>
2026-02-16 18:34:12 -0300

    Update contracts.js
    Update to get all pages at once

--- src/controllers/contracts.js
+++ src/controllers/contracts.js
@@ -44,51 +44,60 @@ const getAllContracts = async (req, res) => {
   const { pagina } = req.body;
 
   try {
-    const body = {
-      call: 'ListarContratos',
-      app_key: process.env.OMIE_APP_KEY,
-      app_secret: process.env.OMIE_APP_SECRET,
-      param: [
-        {
-          "pagina": pagina,
-          "registros_por_pagina": 1000,
-          "apenas_importado_api": "N"
-        }
-      ]
-    }
+    let currentPage = pagina || 1;
+    let totalPages = 1;
+    let contracts;
 
-    const contracts = await instanciaAxiosOmie.post(`servicos/contrato/`, body);
+    while (currentPage <= totalPages) {
+      const body = {
+        call: 'ListarContratos',
+        app_key: process.env.OMIE_APP_KEY,
+        app_secret: process.env.OMIE_APP_SECRET,
+        param: [
+          {
+            "pagina": currentPage,
+            "registros_por_pagina": 100,
+            "apenas_importado_api": "N"
+          }
+        ]
+      }
 
-    for (let item of contracts.data.contratoCadastro) {
-      if (item.cabecalho.cCodSit == 10) { // 10 equivale aos contratos ativos 
-        if (typeof item.departamentos[0] === "undefined") {
-          await knex('contracts')
-            .insert({
-              n_cod_ctr: item.cabecalho.nCodCtr,
-              c_num_ctr: item.cabecalho.cNumCtr,
-              d_vig_inicial: item.cabecalho.dVigInicial,
-              d_vig_final: item.cabecalho.dVigFinal,
-              n_cod_cli: item.cabecalho.nCodCli,
-              n_cod_cc: item.infAdic.nCodCC,
-              n_val_tot_mes: item.cabecalho.nValTotMes,
-              c_cod_dep: 'N/D'
-            });
-        } else {
-          await knex('contracts')
-            .insert({
-              n_cod_ctr: item.cabecalho.nCodCtr,
-              c_num_ctr: item.cabecalho.cNumCtr,
-              d_vig_inicial: item.cabecalho.dVigInicial,
-              d_vig_final: item.cabecalho.dVigFinal,
-              n_cod_cli: item.cabecalho.nCodCli,
-              n_cod_cc: item.infAdic.nCodCC,
-              n_val_tot_mes: item.cabecalho.nValTotMes,
-              c_cod_dep: item.departamentos[0].cCodDep
-            });
+      contracts = await instanciaAxiosOmie.post(`servicos/contrato/`, body);
+
+      totalPages = contracts.data.total_de_paginas;
+
+      for (let item of contracts.data.contratoCadastro) {
+        if (item.cabecalho.cCodSit == 10) { // 10 equivale aos contratos ativos 
+          if (typeof item.departamentos[0] === "undefined") {
+            await knex('contracts')
+              .insert({
+                n_cod_ctr: item.cabecalho.nCodCtr,
+                c_num_ctr: item.cabecalho.cNumCtr,
+                d_vig_inicial: item.cabecalho.dVigInicial,
+                d_vig_final: item.cabecalho.dVigFinal,
+                n_cod_cli: item.cabecalho.nCodCli,
+                n_cod_cc: item.infAdic.nCodCC,
+                n_val_tot_mes: item.cabecalho.nValTotMes,
+                c_cod_dep: 'N/D'
+              });
+          } else {
+            await knex('contracts')
+              .insert({
+                n_cod_ctr: item.cabecalho.nCodCtr,
+                c_num_ctr: item.cabecalho.cNumCtr,
+                d_vig_inicial: item.cabecalho.dVigInicial,
+                d_vig_final: item.cabecalho.dVigFinal,
+                n_cod_cli: item.cabecalho.nCodCli,
+                n_cod_cc: item.infAdic.nCodCC,
+                n_val_tot_mes: item.cabecalho.nValTotMes,
+                c_cod_dep: item.departamentos[0].cCodDep
+              });
+          }
         }
       }
-    }
 
+      currentPage++;
+    }
 
     return res.status(contracts.status).json(contracts.data);
   } catch (error) {
